5cb185b1c3a11ef21b462f2b098510944820bfbc
Jude Kuti <judekuti@gmail.com>
2022-09-06 14:53:44 -0500

    variables strings shell

---
+++ scripts.sh
@@ -0,0 +1,14 @@
+
+
+#variables
+Name="Jude"
+
+# strings
+echo 'Hi $Name' #=> Hi $Name
+echo "Hi $Name" #=> Hi Jude
+echo "Hi ${Name}" #=> Hi Jude
+
+
+#shell execution with backticks
+echo `pwd`
+
